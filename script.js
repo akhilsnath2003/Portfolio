0d7f9f459f8b06770dae1aae0b1eb859ce6fc19f
Lord Asen <akhilsabari2003@gmail.com>
2026-06-27 00:35:47 +0530

    made the web look cool

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// import { gsap } from "gsap/dist/gsap";
+gsap.registerPlugin(SplitText);
+
+let text = new SplitText(".hero", {type:"words,chars"});
+
 gsap.from(".logo",{
     y:-50,
     opacity:0,
@@ -11,4 +16,40 @@ gsap.from(".nav-links li",{
     stagger:0.15,
     ease:"power3.out"
 });
-console.log("Hello World");
+
+gsap.from(text.words,{
+    delay:1.5,
+    scale: 3,
+    y:60,
+    autoAlpha:0,
+    stagger:0.3,
+    // opacity:0,
+    // ease:"power3.out",
+    // duration:1.2,
+    // stagger  : {
+    //     each:0.2,
+    //     from:"end"
+
+    })
+// gsap.registerPlugin(TextPlugin);
+
+// const tl = gsap.timeline({ repeat: -1 });
+
+// tl.to("#typing", {
+//   duration: 1,
+//   text: "BU"
+// })
+// .to({}, { duration: 1 }) // pause
+// .to("#typing", {
+//   duration: 0.8,
+//   text: ""
+// })
+// .to("#typing", {
+//   duration: 1,
+//   text: "BUILD GAMES"
+// })
+// .to({}, { duration: 1 })
+// .to("#typing", {
+//   duration: 0.8,
+//   text: ""
+// });
